1d528b265f585ea00341596c951aad1348bfb9f2
Виталий <39260909+Pandorrumm@users.noreply.github.com>
2022-06-22 12:03:29 +0700

    Add files via upload

---
+++ Tutorial/UIBackgroundHideTutorialScreen.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace GameUI
+{
+    public class UIBackgroundHideTutorialScreen : MonoBehaviour, IPointerClickHandler
+    {
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Tutorial.Instance.NextScreen();
+        }
+    }
+}
