81432e5908738b309afb053c9a801b6269d09dbe
Виталий <39260909+Pandorrumm@users.noreply.github.com>
2022-08-23 11:00:56 +0700

    Delete WayPoint.cs

--- GameLogic/WayPoint.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class WayPoint : MonoBehaviour
-{
-   //public List<GameObject> wayPoints = new List<GameObject>();
-}
